19261680625af9151243ab89de878ff0122e9324
Mindaugas <mcesnauskas@gmail.com>
2024-05-02 11:17:32 +0300

    Add NoteDetails activity

---
+++ app/src/main/java/lt/mindaugas/note_app/NoteDetails.java
@@ -0,0 +1,18 @@
+package lt.mindaugas.note_app;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class NoteDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_details);
+    }
+}
